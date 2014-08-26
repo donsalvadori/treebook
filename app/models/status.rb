4384041cca9d4b46eabbea80d7fdf48c404e5c1e
Chris Salvi <csalvi42@gmail.com>
2014-08-25 22:18:31 -0700

    add status validations and tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,10 @@
 class Status < ActiveRecord::Base
 	belongs_to :user
     validates_presence_of :user
+
+    validates :content, presence: true,
+    						length: { minimum: 2 }
+
+   validates :user_id, presence: true
+
 end
